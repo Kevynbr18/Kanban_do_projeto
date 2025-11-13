aec7016e9d6b9c2ee00cbbc43df7b5909a9e6c20
Kevyn Bruno <165089711+Kevynbr18@users.noreply.github.com>
2025-11-13 19:15:23 +0000

    Removendo todos os arquivos

--- Aluno/MinhasReservas.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".cancelar-btn").forEach(button => {
-  button.addEventListener("click", function () {
-    if (confirm("Tem certeza que deseja cancelar esta reserva?")) {
-      this.closest(".reserva").remove();
-      alert("Reserva cancelada com sucesso!");
-    }
-  });
-});
