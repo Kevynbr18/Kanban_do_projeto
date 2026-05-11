689425c2c8a35f06bbb8d6bf76f761f2a4efe448
KEVYN BRUNO DOS SANTOS LEAL <165089711+Kevynbr18@users.noreply.github.com>
2026-05-11 14:16:35 -0300

    Delete JS directory

--- JS/Feedback.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("meuFormulario").addEventListener("submit", function (e) {
-      e.preventDefault();
-      alert("✅ Obrigado pelo seu feedback! Sua mensagem foi enviada com sucesso.");
-      this.reset();
-    });
